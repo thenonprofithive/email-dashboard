16f8792ada8666e631934a49424c271e2aa91222
Cole Van Vlack <cvanvlack@gmail.com>
2024-11-24 10:22:56 -0500

    Filter out email events from emails that were not sent within the window

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import { useState } from 'react';
 import DashboardForm from './components/Dashboardform';
 import EmailEventsTable from './components/EmailEventsTable';
-import { EmailEvent } from './types/api';
-import { fetchEmailEvents } from './services/api';
+import { EmailEventResponse, EmailEventApiData} from './types/api';
+import { fetchEmailEvents, fetchEmailInfo } from './services/api';
 import './App.css';
-import { EmailEventData } from './types/api'
 
 const App = () => {
-  const [emailData, setEmailData] = useState<EmailEvent[] | null>(null);
+  const [emailData, setEmailData] = useState<EmailEventResponse[] | null>(null);
 
-  const handleSubmit = async (formData: EmailEventData) => {
+  const handleSubmit = async (formData: EmailEventApiData) => {
     try {
-      const data = await fetchEmailEvents(formData);
-      setEmailData(data);
+      const emailEventsData = await fetchEmailEvents(formData);
+      const emailInfoData = await fetchEmailInfo(formData);
+      //emailEventsData is an array of all of the email events. However we only want email events associated with emails that were sent between the start date and the end date. We need to loop throught the array of emailEventsData, and then loop through the array of emailInfoData to find the email events that correspond with the emailInfoData.
+      const emailEventsDataWithInfo: EmailEventResponse[] = [];
+      for (const emailEvent of emailEventsData) {      
+        for (const emailInfo of emailInfoData) {
+          if (emailEvent.messageId === emailInfo.messageId) {
+            if (emailInfo.date) {
+              if (emailInfo.date >= formData.startDate && emailInfo.date <= formData.endDate) {
+                emailEvent.dateSent = emailInfo.date;
+                emailEventsDataWithInfo.push(emailEvent);
+              }
+            }
+          }
+        }
+      }
+      setEmailData(emailEventsDataWithInfo);
     } catch (error) {
       console.error('Error fetching data:', error);
       alert('Error fetching data. Please try again.');
